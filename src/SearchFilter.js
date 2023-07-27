c63c36d1bc80f8b81b57783599fa4a0e604db20f
Monday_David_Sarka <david_md@hamari.agency>
2023-07-27 17:48:11 +0100

    Added search feature by email and username too

--- src/SearchFilter.js
+++ src/SearchFilter.js
@@ -2,57 +2,69 @@ import React, { useEffect, useState } from "react";
 
 export default function SearchFilter() {
   const [data, setData] = useState([]);
-  const [searchApiData, setSearchApiData] = useState([])
-  const [filterVal, setFilterVal] = useState('')
-  
+  const [searchApiData, setSearchApiData] = useState([]);
+  const [filterVal, setFilterVal] = useState("");
+
   useEffect(() => {
     const fetchData = () => {
       fetch("https://jsonplaceholder.typicode.com/users")
         .then((response) => response.json())
         .then((json) => {
-          setData(json)
-          setSearchApiData(json)
+          setData(json);
+          setSearchApiData(json);
         });
-    }
+    };
     fetchData();
-  }, [])
+  }, []);
 
   const handleFilter = (e) => {
-    if(e.target.value === ''){
-      setData(searchApiData)
-    }else {
-      const filterResult = searchApiData.filter(item => item.name.toLowerCase().includes(e.target.value.toLowerCase()))
-      setData(filterResult)
+    if (e.target.value === "") {
+      setData(searchApiData);
+    } else {
+      const filterResult = searchApiData.filter(
+        (item) =>
+          item.name.toLowerCase().includes(e.target.value.toLowerCase()) ||
+          item.email.toLowerCase().includes(e.target.value.toLowerCase()) ||
+          item.username.toLowerCase().includes(e.target.value.toLowerCase())
+      );
+      if (filterResult.length > 0) {
+        setData(filterResult);
+      } else {
+        setData([{ email: "No Data Found" }]);
+      }
     }
 
-    setFilterVal(e.target.value)
-  }
+    setFilterVal(e.target.value);
+  };
 
   return (
     <div className="p-2 px-8">
       <div className="w-full">
-        <input placeholder="Search" value={filterVal} onInput={(e)=> handleFilter(e)} className="border outline-2 outline-orange-400 p-2" />
+        <input
+          placeholder="Search"
+          value={filterVal}
+          onInput={(e) => handleFilter(e)}
+          className="border outline-2 outline-orange-400 p-2"
+        />
       </div>
       <table className="w-full p-2">
         <tbody>
-          <tr className="text-left border-b-4 border-cyan-400 mb-2">         
+          <tr className="text-left border-b-4 border-cyan-400 mb-2">
             <th>Name</th>
             <th>UserName</th>
             <th>Email</th>
             <th>Phone</th>
           </tr>
-          {
-            data.map(item => {
-              return (
-                <tr className="border-b border-cyan-400 mt-8">
-                  <td>{item.name}</td>
-                  <td>{item.username}</td>
-                  <td>{item.email}</td>
-                  <td>{item.phone}</td>
-                </tr>
-              )
-            })
-          }
+          {data.map((item) => {
+            return (
+              <tr className="border-b border-cyan-400 mt-8">
+                <td>{item.name}</td>
+                <td>{item.username}</td>
+                <td>{item.email}</td>
+                <td>{item.phone}</td>
+              </tr>
+            );
+          })}
         </tbody>
       </table>
     </div>
