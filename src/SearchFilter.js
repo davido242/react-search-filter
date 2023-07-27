4f9444b19512e7eccd041d107e84cc8f387306dd
Monday_David_Sarka <david_md@hamari.agency>
2023-07-27 12:05:29 +0100

    Pulled data to front end using fetch api and useState/useEffect hooks

--- src/SearchFilter.js
+++ src/SearchFilter.js
@@ -1,10 +1,42 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 
 export default function SearchFilter() {
+  const [data, setData] = useState([]);
+  
+  useEffect(() => {
+    const fetchData = () => {
+      fetch("https://jsonplaceholder.typicode.com/users")
+        .then((response) => response.json())
+        .then((json) => {
+          setData(json)
+        });
+    }
+    fetchData();
+  }, []);
   return (
     <div>
-        <h2>Seach by spelling tips</h2>
-      
+      <table>
+        <tbody>
+          <tr>            
+          <th>Name</th>
+          <th>UserName</th>
+          <th>Email</th>
+          <th>Phone</th>
+          </tr>
+          {
+            data.map(item => {
+              return (
+                <tr>
+                  <td>{item.name}</td>
+                  <td>{item.id}</td>
+                  <td>{item.email}</td>
+                  <td>{item.phone}</td>
+                </tr>
+              )
+            })
+          }
+        </tbody>
+      </table>
     </div>
-  )
+  );
 }
